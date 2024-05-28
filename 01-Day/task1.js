c7ad1a1da95e9e4e1e9166a09e70fedde2b89286
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-05-28 22:55:36 +0500

    Question 1

---
+++ 01-Day/task1.js
@@ -0,0 +1,6 @@
+/*
+* Question 1: Personal Message: Store a person’s name in a variable and print a message to them,
+* like “Hello Aftab Hussain, would you like to learn some TypeScript today?”
+*/
+var personName = "Aftab Hussain";
+console.log("Hello ".concat(personName, ", would you like to learn some TypeScript today?"));
---
+++ 01-Day/task1.ts
@@ -0,0 +1,8 @@
+/*
+* Question 1: Personal Message: Store a person’s name in a variable and print a message to them, 
+* like “Hello Aftab Hussain, would you like to learn some TypeScript today?”
+*/
+
+const personName: string = "Aftab Hussain";
+
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
