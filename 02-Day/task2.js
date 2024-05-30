8e78e33b2e72e0271582e6b6d9b3075ba37b2a5a
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-05-30 05:06:36 +0500

    task2

--- 02-Day/task2.js
+++ 02-Day/task2.js
@@ -2,6 +2,6 @@
 * Question 2: Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person.
 * Then compose your message and store it in a new variable called message. Print your message.
 */
-let famousPerson = "Albert Einstein";
-let message = (`${famousPerson} once said, “A person who never made a mistake never tried anything new.”`);
+var famousPerson = "Albert Einstein";
+var message = ("".concat(famousPerson, " once said, \u201CA person who never made a mistake never tried anything new.\u201D"));
 console.log(message);
