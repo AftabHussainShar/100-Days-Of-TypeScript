391f9a0a5dd13746ac022bddfc33f7c8ee5fc528
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-06-08 10:54:23 +0500

    Task 2

--- 03-Day/task1.ts
+++ 03-Day/task1.ts
@@ -1,5 +1,5 @@
 /*
-* Number Eight: Write addition, subtraction, multiplication,
+* Write addition, subtraction, multiplication,
 * and division operations that each result in the number 8.
 * Enclose your operations in print statements.
 */
---
+++ 03-Day/task2.ts
@@ -0,0 +1,6 @@
+/*
+* Favorite Number: Store your favorite number in a variable.
+* Then, create a message revealing your favorite number. Print that message.
+*/
+let favoriteNumber = 4;
+console.log(`My favorite no is ${favoriteNumber}`);
